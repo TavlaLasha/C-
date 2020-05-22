011369c3797836ed810865c821566bf5b86d9009
TavlaLasha <lashatavlalashvili@yahoo.com>
2020-05-22 22:17:00 +0400

    Homework

---
+++ Davaleba 7/Linq/Linq/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    class Product
+    {
+        public int id { set; get; }
+        public string name { set; get; }
+        public double price { set; get; }
+        public static List<Product> GetAllProducts()
+        {
+            List<Product> products = new List<Product>()
+                {
+                    new Product {id=1, name="Fanta", price=2.50},
+                    new Product {id=2, name="Coca-Cola", price=2.70},
+                    new Product {id=3, name="Sprite", price=2.60},
+                    new Product {id=4, name="Boom", price=1.00},
+                    new Product {id=5, name="Pepsi", price=2.75}
+                };
+            return products;
+        }
+    }
+}
---
+++ Davaleba 7/Linq/Linq/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            double TotalProfit = Product.GetAllProducts().Sum(std =>std.price);
+            double MostExpensive = Product.GetAllProducts().Max(std => std.price);
+            double Cheapest = Product.GetAllProducts().Min(std => std.price);
+            //bool higherthantwo = Product.GetAllProducts().Any(std => std.price>2);
+
+            Console.Write($"TotalProfit: {TotalProfit}\n");
+            Console.Write($"Most Expensive: {MostExpensive}\n");
+            Console.Write($"Cheapest: {Cheapest}\n");
+
+            Console.ReadKey();
+        }
+    }
+}
